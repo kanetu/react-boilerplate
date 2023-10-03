30362bc7271535af946ee9eaadaee31053958c5e
Kane Tu <kanetu731@gmail.com>
2023-10-03 15:44:19 +0700

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React, { useEffect } from "react";
 import "App.scss";
-import Button from "@components/Button/Button";
 import tw from "twin.macro";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Profile from "@features/Profile";
 
 const Input = tw.input`border hover:border-blue-50 p-5 m-4`;
 
@@ -12,12 +19,30 @@ const App: React.FC = () => {
     }).then((res) => console.log(res));
   }, []);
 
+  console.log(Input);
+
   return (
     <div className="main">
-      <Button>Hello newcommer!</Button>
-      <Input></Input>
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<AuthRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+        {/* <Footer /> */}
+      </BrowserRouter>
     </div>
   );
+};
+
+const AuthRoute: React.FC = () => {
+  const isAuthenticated = true;
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default App;
